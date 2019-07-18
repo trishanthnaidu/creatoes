5cd3a0e896f6d932b688e880ccd49be12feb9fd8
ranganathan v naidu <ranganathanvnaidu@ranganathans-MacBook-Air.local>
2019-07-18 10:06:49 +0530

    Updates

---
+++ precache-manifest.6ce44e09243b7472925830f328de2541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d9ef60dc44f3c37f330a4bcda87ab9",
+    "url": "/trishanthnaidu/creatoes/index.html"
+  },
+  {
+    "revision": "571d48f69b1ba5f78d3b",
+    "url": "/trishanthnaidu/creatoes/static/css/2.1ad9c816.chunk.css"
+  },
+  {
+    "revision": "571d48f69b1ba5f78d3b",
+    "url": "/trishanthnaidu/creatoes/static/js/2.7a62392e.chunk.js"
+  },
+  {
+    "revision": "346c423ff8faebb2fc59",
+    "url": "/trishanthnaidu/creatoes/static/js/main.f4af8c46.chunk.js"
+  },
+  {
+    "revision": "834141363691954f4a4f",
+    "url": "/trishanthnaidu/creatoes/static/js/runtime~main.ebe28045.js"
+  }
+]);
